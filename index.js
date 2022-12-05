27115ad6fba521d1f7132c0108b3e906a55d0a84
muhammadanang <muhanangmahrub@gmail.com>
2022-12-05 14:44:13 +0700

    connecting to database

---
+++ index.js
@@ -0,0 +1,40 @@
+import express from 'express'
+import cors from 'cors'
+import MoviesRoute from './api/MoviesRoute.js'
+import dotenv from 'dotenv'
+import mongodb from 'mongodb'
+
+class Index {
+    static app = express()
+    static router = express.Router()
+    static main() {
+        dotenv.config()
+        Index.setUpServer()
+        Index.setUpDatabase()
+    }
+
+    static setUpServer() {
+        Index.app.use(cors())
+        Index.app.use(express.json())
+        Index.app.use('/api/v1/movies', MoviesRoute.configRoutes(Index.router))
+        Index.app.use('*', (req, res) => {
+            res.status(404).json({ error: 'not found' })
+        })
+    }
+
+    static async setUpDatabase() {
+        const client = new mongodb.MongoClient(process.env.MOVIEREVIEWS_DB_URI)
+        const port = process.env.PORT || 8000
+        try {
+            await client.connect()
+            Index.app.listen(port, () => {
+                console.log(`server is running on port:${port}`)
+            })
+        } catch (e) {
+            console.error(e)
+            process.exit(1)
+        }
+    }
+}
+
+Index.main()
